1882363a64c73fd6c863b27a6cfad422dc8b2871
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-04-02 22:44:27 -0300

    se resuelve conflicto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Header } from './components/header/header'
 import { AppRouter } from './routes'
 
 function App() {
 
   return (
     <>
+     <Header></Header>
      <AppRouter/>
     </>
   )
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,11 @@
-import { useNavigate } from 'react-router-dom'
+
 import './header.css'
 
-
 export const Header = () => {
-    const navigate = useNavigate()
-
-    const gotTo = () => { navigate('/home') }
 
     return(
         <>
-            <header onClick={gotTo} className='header'>Event Nexus</header>
+            <header className="header"> Event Nexus </header>
         </>
     )
 }
