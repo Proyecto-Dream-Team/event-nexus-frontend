319e605c3faaed5377b678fe1c0d6c3c4f8b89cc
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-04-04 20:40:27 -0300

    diseño inicial del home

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,13 +1,19 @@
 import { Header } from "../components/header/header"
 import { NavBar } from "../components/navbar/navbar"
-
+import './home.css'
 export const Home = () => {
-
 
     return(
         <>
             <Header></Header>
-                <p>Modulos</p>
+                <h1>Modulos</h1>
+
+                <main className="button-grid">
+                    <div className="button-square"></div>
+                    <div className="button-square"></div>
+                    <div className="button-square"></div>
+                    <div className="button-square"></div>
+                </main>
             <NavBar></NavBar>
         </>
     )
