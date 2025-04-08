b0678dd936ec3af1a169fa6a7c481eef9d221e2b
Theo Narmontas <narmontastheo@gmail.com>
2025-04-08 00:47:26 -0300

    pagina de login

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { InputApp } from '../components/input/input'
+
 
 
 export const Login = () => {
-
-
+    const [userName, setUser] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
 
     return(
-        <h1>Event Nexus</h1>
+        <div style={{justifyContent:'center', alignItems: 'center', justifyItems:'center'}}>
+            <h1 style={{marginTop:'50px', marginBottom:'10px'}}>Event Nexus</h1>
+            <hr></hr>
+
+            <Box sx={{backgroundColor:'#7289DA', width:'90vmin', height:'100vmin', position:'relative', justifyContent:'center', alignItems: 'center', justifyItems:'center', margin:'5vmin', maxHeight:'500px', maxWidth:'500px', borderRadius:'10%'}}>
+                <br></br>
+                <InputApp label='usuario' type='text' register={true}></InputApp>
+                <InputApp label='contraseña' type='text' register={true}></InputApp>
+
+                <button>Iniciar Sesion</button>
+            </Box>
+        </div>
     )
 
 }
