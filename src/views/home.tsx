4906c9bd069aaeb7767aca5faf682a8f9974ba02
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-04-08 22:18:56 -0300

    creacion del layout

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,20 +1,16 @@
-import { Header } from "../components/header/header"
-import { NavBar } from "../components/navbar/navbar"
 import './home.css'
 export const Home = () => {
 
     return(
         <>
-            <Header></Header>
-                <h1>Modulos</h1>
+            <h1>Modulos</h1>
 
-                <main className="button-grid">
-                    <div className="button-square"></div>
-                    <div className="button-square"></div>
-                    <div className="button-square"></div>
-                    <div className="button-square"></div>
-                </main>
-            <NavBar></NavBar>
+            <main className="button-grid">
+                <div className="button-square"></div>
+                <div className="button-square"></div>
+                <div className="button-square"></div>
+                <div className="button-square"></div>
+            </main>
         </>
     )
 
