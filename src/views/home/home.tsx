27e320feea6d9e0ad858cbe3ec29736018211e46
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-04-09 15:08:02 -0300

    se agrandan las imagenes y se agrega el divider

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,11 +1,19 @@
+import { Divider } from '@mui/material'
 import { Module } from '../../components/module/module'
 import './home.css'
+
+const dividerStyles = {
+    backgroundColor: '#ffffff',
+    height: '2px',
+    margin: '2rem 4rem',
+  }
+
 export const Home = () => {
 
     return(
         <>
             <h1>Modulos</h1>
-
+            <Divider style={dividerStyles} />
             <main className="button-grid">
                 <Module></Module>
                 <Module></Module>
