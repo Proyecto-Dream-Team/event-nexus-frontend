2dedd2347b05824826d88d0c340b2dbd24d3e446
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-05-04 22:38:18 -0300

    borro el mock

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -10,15 +10,6 @@ export const Home = () => {
     const [ modules , setModules ] = useState<Module[]>()
     const id = Number(sessionStorage.getItem("userId"));
 
-
-    const modulo1 = new Module( 0 , "Events" ,"Lorem ipsum dolor sit amet consectetur adipisicing elit.", "events.svg")
-    const modulo2 = new Module( 1 , "Directivas" ,"Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia porro numquam blanditiis itaque.", "information.svg")
-    const modulo3 = new Module( 2 , "Reservar Espacio", "Lorem ipsum dolor sit amet consectetur adipisicing elit." , "reservarEspacio.png")
-    const modulo4 = new Module( 3 , "pepe" ,"Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia porro numquam blanditiis itaque.", "reservarEspacio.png")
-
-
-    const modulos = [ modulo1 , modulo2 , modulo3 , modulo4 ]
-
     const getModules = async () => {
         try{
             const res = await moduleService.getModules(id)
