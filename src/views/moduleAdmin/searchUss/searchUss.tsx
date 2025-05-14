8e29f8d518fa2ba75c22c19ced531c374d429f4c
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-05-14 20:07:08 -0300

    search style no muy conforme

--- src/views/moduleAdmin/searchUss/searchUss.tsx
+++ src/views/moduleAdmin/searchUss/searchUss.tsx
@@ -1,6 +1,22 @@
-export const SearchUser = () => {
-      return (
-        <h1>Search User</h1>
-    )
+import React, { useState } from "react";
+import "./searchUss.css";
 
-}
+
+export const SearchUser = () => {
+  const [value, setValue] = useState("");
+
+  return (
+    <section className="section">
+      <div className="search__container">
+        <input
+          className="search__input"
+          type="text"
+          placeholder="Search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+    </section>
+  );
+};
+
