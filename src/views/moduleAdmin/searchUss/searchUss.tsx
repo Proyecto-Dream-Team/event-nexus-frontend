d98baec4294c41eaa8493e9a2712d2c8af269654
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-05-14 20:50:44 -0300

    segunda opcion

--- src/views/moduleAdmin/searchUss/searchUss.tsx
+++ src/views/moduleAdmin/searchUss/searchUss.tsx
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
 import "./searchUss.css";
-
 
 export const SearchUser = () => {
   const [value, setValue] = useState("");
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <section className="section">
-      <div className="search__container">
+      <div
+        className={`search-container ${expanded ? "expanded" : ""}`}
+        onClick={() => setExpanded(true)}
+      >
         <input
-          className="search__input"
           type="text"
-          placeholder="Search"
+          name="search"
+          placeholder="Search..."
+          className="search-input"
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
+        <SearchIcon className="search-icon" />
       </div>
     </section>
   );
 };
-
