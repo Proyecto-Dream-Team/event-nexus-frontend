852bd16ba9020b7239829d95ffaa4b1d500b570b
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-06-03 18:31:44 -0300

    modifico DTO directive

--- src/domain/directiveInfo.ts
+++ src/domain/directiveInfo.ts
@@ -1,15 +1,19 @@
+type Priority = "HIGH" | "MEDIUM" | "LOW" 
+
 export class DirectiveInfoData {
     creatorId: number;
     creatorImage: string;
     title: string;
     date: string;
     description: string;
+    priority: Priority
 
-    constructor(idCreator: number, creatorImage:string, titulo: string, fecha: string, descripcion: string) {
+    constructor(idCreator: number, creatorImage:string, titulo: string, fecha: string, descripcion: string, priority : Priority) {
         this.creatorId = idCreator;
         this.creatorImage = creatorImage;
         this.title = titulo;
         this.date = fecha;
         this.description = descripcion;
+        this.priority = priority
     }
 }
