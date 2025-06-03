925cbb479a718f7d9d7dcae16ac7eb179f1f2110
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-06-03 18:05:44 -0300

    cambios

--- src/views/moduleDirectiveInfo/DirectiveInfo/informacionDirectiva.tsx
+++ src/views/moduleDirectiveInfo/DirectiveInfo/informacionDirectiva.tsx
@@ -3,6 +3,8 @@ import { DirectiveInfoData } from '../../../domain/directiveInfo'
 import { CardInfoDirectiva } from './cardInfoDirectiva'
 import './informacionDirectiva.css'
 import { AccountCircle } from '@mui/icons-material'
+import { useEffect, useState } from 'react'
+import { serviceUser } from '../../../services/serviceUser'
 
 const mockDirective = new DirectiveInfoData(
     1,
@@ -15,9 +17,26 @@ const mockDirective = new DirectiveInfoData(
 const directives = [mockDirective,mockDirective,mockDirective,mockDirective]
 
 export const InformacionDirectiva = () => {
+
+    const [canCreate, setCanCreate] = useState(false);
+
+    async function getPermissions() {
+        try{
+            const userId = Number(sessionStorage.getItem("userId"));
+            const permissions = await serviceUser.getPermissionsUser(userId, "DIRECTIVE");
+            setCanCreate(permissions.length != 0);
+        }
+        catch (error: any) {
+            console.error("Error al obtener permisos: ", error.message);
+        }
+    }
+    useEffect(() => {
+        getPermissions()
+    }, []);
     return (
         <>
             <div className="fixed-background" />
+            <button disabled={!canCreate}>boton</button>
             <div className="scrollable-content">
                 {directives.map((directive, index) => (
                     <CardInfoDirectiva key={index} value={directive} />
