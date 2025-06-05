be6b82d1b12dd6b285c831609d15bd0a8a7d9f50
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-06-05 16:00:33 -0300

    arreglo estilos de pica

--- src/components/viewLayout/viewLayout.tsx
+++ src/components/viewLayout/viewLayout.tsx
@@ -7,6 +7,6 @@ export const ViewLayout = () => {
     return <>
         <Header />
         <Outlet />
-        <NavBar />
+        {/* <NavBar /> */}
     </>
 };
--- src/views/moduleDirectiveInfo/DirectiveInfo/directiveInfo.tsx
+++ src/views/moduleDirectiveInfo/DirectiveInfo/directiveInfo.tsx
@@ -8,31 +8,31 @@ import { Box, Button, Chip, TextField } from '@mui/material'
 
 export const DirectiveInfo = () => {
 
-    const [directive,setDirective] = useState<DirectiveInfoData[]>([])
+    const [directive, setDirective] = useState<DirectiveInfoData[]>([])
 
     const [canCreate, setCanCreate] = useState(false);
 
     const [title, setTitle] = useState('');
-    
+
     const [description, setDescription] = useState('');
-    
+
     const [priority, setPriority] = useState<'URGENTE' | 'IMPORTANTE' | 'INFORMATIVO' | null>(null);
-    
+
     const userId = Number(sessionStorage.getItem("userId"));
-    
+
     const createDirective = async () => {
         if (!priority || !title || !description) {
-          alert("Completa todos los campos y selecciona una prioridad");
-          return;
+            alert("Completa todos los campos y selecciona una prioridad");
+            return;
         }
-      
+
         const newDirective = new DirectiveInfoData(
             userId,
             title,
             description,
-            priority.toLowerCase() as Priority 
+            priority.toLowerCase() as Priority
         );
-      
+
         try {
             await directiveInfoService.createDirectiveInfo(newDirective);
             resetValues()
@@ -40,25 +40,25 @@ export const DirectiveInfo = () => {
             console.error("Error al crear directiva:", error.message);
         }
     };
-    
+
     const resetValues = () => {
-        getDirectives(); 
+        getDirectives();
         setDescription('');
         setTitle('')
         setPriority(null);
     }
-    
+
     const getDirectives = async () => {
-        try{
+        try {
             const res = await directiveInfoService.fetchDirectives()
             setDirective(res)
-        }catch (error: any) {
+        } catch (error: any) {
             console.error("Error al obtener permisos: ", error.message);
         }
-    } 
+    }
 
     async function getPermissions() {
-        try{
+        try {
             const permissions = await serviceUser.getPermissionsUser(userId, "DIRECTIVE");
             setCanCreate(permissions.length != 0);
         }
@@ -74,79 +74,81 @@ export const DirectiveInfo = () => {
 
     return (
         <>
-            <div className="fixed-background" />
-            <section className="scrollable-content">
-                {directive.map((directive, index) => (
-                    <CardDirectiveInfo key={index} value={directive} />
-                ))}
-            </section>
-            {canCreate ? (
-                <>
-                    <div className='create-directive'>
-                        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                            <Box sx={{  width: '80%' , display: 'flex' , flexDirection: 'column' , gap: 2}}>
-                                
-                            <TextField
-                                    label="titulo"
-                                    multiline
-                                    minRows={1}
-                                    fullWidth
-                                    value={title}
-                                    onChange={(e) => setTitle(e.target.value)}
-                                    sx={{
-                                        backgroundColor: 'white',
-                                        borderRadius: '1rem',
-                                    }}
-                                />
-                                <TextField
-                                    label="Descripción"
-                                    multiline
-                                    minRows={2}
-                                    fullWidth
-                                    value={description}
-                                    onChange={(e) => setDescription(e.target.value)}
-                                    sx={{
-                                        backgroundColor: 'white',
-                                        borderRadius: '1rem',
-                                    }}
-                                />
-                                <Box sx={{ display: 'flex', justifyContent: 'center', my: 1 }}>
-                                    <Box sx={{ display: 'flex', gap: 2 }}>
-                                        <Chip
-                                        label="Urgente"
-                                        color="error"
-                                        variant={priority === 'URGENTE' ? 'filled' : 'outlined'}
-                                        onClick={() => setPriority('URGENTE')}
-                                        />
-                                        <Chip
-                                        label="Importante"
-                                        color="warning"
-                                        variant={priority === 'IMPORTANTE' ? 'filled' : 'outlined'}
-                                        onClick={() => setPriority('IMPORTANTE')}
-                                        />
-                                        <Chip
-                                        label="Informativo"
-                                        color="primary"
-                                        variant={priority === 'INFORMATIVO' ? 'filled' : 'outlined'}
-                                        onClick={() => setPriority('INFORMATIVO')}
-                                        />
-                                    </Box>
-                                </Box>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={createDirective}
-                                    sx={{ borderRadius: '1rem', whiteSpace: 'nowrap' }}
-                                    >
-                                    Crear
-                                </Button>
-                            </Box>
-                        </Box>
+            <div>ACA IRIA FILTER</div>
+            <div className='scrollable-adrian'>
+                <section className="scrollable-content">
+                    {directive.map((directive, index) => (
+                        <CardDirectiveInfo key={index} value={directive} />
+                    ))}
+                </section>
+                {canCreate ? (
+                    <>
+                        <div className='create-directive'>
+                            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                                <Box sx={{ width: '80%', display: 'flex', flexDirection: 'column', gap: 2 }}>
 
-                    </div>
-                    
-                </>
+                                    <TextField
+                                        label="titulo"
+                                        multiline
+                                        minRows={1}
+                                        fullWidth
+                                        value={title}
+                                        onChange={(e) => setTitle(e.target.value)}
+                                        sx={{
+                                            backgroundColor: 'white',
+                                            borderRadius: '1rem',
+                                        }}
+                                    />
+                                    <TextField
+                                        label="Descripción"
+                                        multiline
+                                        minRows={2}
+                                        fullWidth
+                                        value={description}
+                                        onChange={(e) => setDescription(e.target.value)}
+                                        sx={{
+                                            backgroundColor: 'white',
+                                            borderRadius: '1rem',
+                                        }}
+                                    />
+                                    <Box sx={{ display: 'flex', justifyContent: 'center', my: 1 }}>
+                                        <Box sx={{ display: 'flex', gap: 2 }}>
+                                            <Chip
+                                                label="Urgente"
+                                                color="error"
+                                                variant={priority === 'URGENTE' ? 'filled' : 'outlined'}
+                                                onClick={() => setPriority('URGENTE')}
+                                            />
+                                            <Chip
+                                                label="Importante"
+                                                color="warning"
+                                                variant={priority === 'IMPORTANTE' ? 'filled' : 'outlined'}
+                                                onClick={() => setPriority('IMPORTANTE')}
+                                            />
+                                            <Chip
+                                                label="Informativo"
+                                                color="primary"
+                                                variant={priority === 'INFORMATIVO' ? 'filled' : 'outlined'}
+                                                onClick={() => setPriority('INFORMATIVO')}
+                                            />
+                                        </Box>
+                                    </Box>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={createDirective}
+                                        sx={{ borderRadius: '1rem', whiteSpace: 'nowrap' }}
+                                    >
+                                        Crear
+                                    </Button>
+                                </Box>
+                            </Box>
+
+                        </div>
+
+                    </>
                 ) : null}
+            </div>
         </>
-      );
+    );
 }
